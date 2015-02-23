b07915421c275ee97cc16343473636fd3eb82e18
Hashcode <hashcode0f@gmail.com>
2015-02-22 23:59:24 -0800

    ion: change to custom+1 HEAP id's vs. unique omap enum
    Change-Id: Ic86fb387f5795f401bbfb61f2d5a01d9a87af8fb

--- arch/arm/mach-omap2/omap4_ion.c
+++ arch/arm/mach-omap2/omap4_ion.c
@@ -47,22 +47,22 @@ static struct ion_platform_heap omap4_ion_heaps[] = {
 		.id = OMAP_ION_HEAP_SECURE_INPUT,
 		.name = "secure_input",
 	},
-	{	.type = OMAP_ION_HEAP_TYPE_TILER,
+	{	.type = OMAP_ION_HEAP_TILER,
 		.id = OMAP_ION_HEAP_TILER,
 		.name = "tiler",
 	},
 	{
-		.type = OMAP_ION_HEAP_TYPE_TILER,
+		.type = OMAP_ION_HEAP_TILER,
 		.id = OMAP_ION_HEAP_NONSECURE_TILER,
 		.name = "nonsecure_tiler",
 	},
 	{
 		.type = ION_HEAP_TYPE_SYSTEM,
-		.id = OMAP_ION_HEAP_SYSTEM,
+		.id = ION_HEAP_TYPE_SYSTEM,
 		.name = "system",
 	},
 	{
-		.type = OMAP_ION_HEAP_TYPE_TILER_RESERVATION,
+		.type = OMAP_ION_HEAP_TILER_RESERVATION,
 		.id = OMAP_ION_HEAP_TILER_RESERVATION,
 		.name = "tiler_reservation",
 	},
@@ -182,7 +182,7 @@ void __init omap_ion_init(void)
 
 	for (i = 0; i < omap4_ion_data.nr; i++)
 		if (omap4_ion_data.heaps[i].type == ION_HEAP_TYPE_CARVEOUT ||
-		    omap4_ion_data.heaps[i].type == OMAP_ION_HEAP_TYPE_TILER) {
+		    omap4_ion_data.heaps[i].type == OMAP_ION_HEAP_TILER) {
 			ret = memblock_remove(omap4_ion_data.heaps[i].base,
 					      omap4_ion_data.heaps[i].size);
 			if (!omap4_ion_data.heaps[i].size)
